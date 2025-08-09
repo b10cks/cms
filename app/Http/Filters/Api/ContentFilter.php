bfe9e4cbb9f6cd7f301065f50ba187a1da6106af
Michael Wallner <m.wallner@coderscantina.com>
2025-08-09 10:04:14 +0200

    🩹 update content filter to use qualified column names

--- app/Http/Filters/Api/ContentFilter.php
+++ app/Http/Filters/Api/ContentFilter.php
@@ -9,31 +9,31 @@ class ContentFilter extends AdvancedFilter
 {
     public function published_at($value)
     {
-        $this->applyAdvancedRangeFilter('published_at', $value);
+        $this->applyAdvancedRangeFilter('contents.published_at', $value);
     }
 
     public function updated_at($value)
     {
-        $this->applyAdvancedRangeFilter('updated_at', $value);
+        $this->applyAdvancedRangeFilter('contents.updated_at', $value);
     }
 
     public function created_at($value)
     {
-        $this->applyAdvancedRangeFilter('created_at', $value);
+        $this->applyAdvancedRangeFilter('contents.created_at', $value);
     }
 
     public function language($value)
     {
-        $this->builder->where('language_iso', $value);
+        $this->builder->where('contents.language_iso', $value);
     }
 
     public function content_type($value)
     {
-        $this->builder->whereIn('block_id', Block::where('type', $value));
+        $this->builder->whereIn('contents.block_id', Block::where('type', $value));
     }
 
     public function parent_id($value)
     {
-        $this->builder->where('parent_id', $value);
+        $this->builder->where('contents.parent_id', $value);
     }
 }
