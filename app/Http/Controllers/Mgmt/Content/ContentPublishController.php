c63eb6bc3c6e5739ee7de55aafcedffbda00b3d7
Michael Wallner <m.wallner@coderscantina.com>
2026-01-26 14:44:45 +0100

    ⚰️ Remove unused commented-out code

--- app/Http/Controllers/Mgmt/Content/ContentPublishController.php
+++ app/Http/Controllers/Mgmt/Content/ContentPublishController.php
@@ -18,11 +18,6 @@ class ContentPublishController extends Controller
         $data = $request->validated();
         $action->execute($data, $content, $space, $request->user());
 
-        //        if (!$content->save()) {
-//            Log::error('Failed to publish content', ['content_id' => $content->id, 'space_id' => $space->id]);
-//            abort(500, 'Failed to publish content');
-//        }
-
         $content->load(['block', 'parent', 'i18n_parent', 'i18n_children', 'i18n_siblings', 'current_version']);
 
         return new ContentResource($content);
