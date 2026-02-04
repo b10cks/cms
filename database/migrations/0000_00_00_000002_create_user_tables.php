fd134785c9c13ef214ff4bd5cdda903d1323cf2b
Michael Wallner <m.wallner@coderscantina.com>
2026-02-04 15:00:36 +0100

    🗃️ Move 2FA fields to initial users migration

--- database/migrations/0000_00_00_000002_create_user_tables.php
+++ database/migrations/0000_00_00_000002_create_user_tables.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
@@ -18,6 +17,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_backup_codes')->nullable();
+            $table->timestamp('two_factor_enabled_at')->nullable();
 
             $table->string('source')->charset('ascii')->default('website');
             $table->string('language_iso', 5)->charset('ascii')->default('en');
