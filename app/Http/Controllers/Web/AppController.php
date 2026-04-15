4641e373786f3200cddfa0f0abcc2e99fdc33935
Michael Wallner <m.wallner@coderscantina.com>
2026-04-15 19:54:17 +0200

    ⚡️ Send cache header for web response

--- app/Http/Controllers/Web/AppController.php
+++ app/Http/Controllers/Web/AppController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AppController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('app');
+        $etag = '"' . config('app.version') . '"';
+
+        if ($request->header('If-None-Match') === $etag) {
+            return response('', Response::HTTP_NOT_MODIFIED)
+                ->header('ETag', $etag)
+                ->header('Cache-Control', 'public, max-age=604800, s-maxage=600, must-revalidate');
+        }
+
+        return response()
+            ->view('app')
+            ->header('ETag', $etag)
+            ->header('Cache-Control', 'public, max-age=604800, s-maxage=600, must-revalidate');
     }
 }
