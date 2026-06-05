cefdda12f2e0871a8798bd03ccf1b987fe745388
Michael Wallner <m.wallner@coderscantina.com>
2026-06-05 21:34:29 +0200

    🐛 Another use of parent_id in slug uniqueness validation

--- app/Http/Requests/Content/UpsertContentRequest.php
+++ app/Http/Requests/Content/UpsertContentRequest.php
@@ -46,7 +46,7 @@ class UpsertContentRequest extends FormRequest
                 'max:70',
                 Rule::unique($connectionName . '.contents', 'slug')
                     ->when($languageIso, fn($query) => $query->where('language_iso', $languageIso))
-                    ->when($parentId, fn($query) => $query->where('parent_id', $parentId))
+                    ->when($parentId, fn($query) => $query->where('parent_id', $parentId), fn($query) => $query->whereNull('parent_id'))
                     ->whereNull('deleted_at')
                     ->ignore($contentId),
             ],
